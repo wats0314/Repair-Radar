5a149115df991b85f40ce156fa5e8752cdd48d9b
Mitch Watson <wats0314@d.umn.edu>
2023-04-04 00:20:34 -0500

    git added spin and color and delete item

--- scripts.js
+++ scripts.js
@@ -5,45 +5,43 @@ const SHEET_ID = '1HwbGp-beLvqEEEVCwOI_TzovZv8YRc5VQQj4fQ8V-vk'; // Replace with
 const API_KEY = 'AIzaSyBv0UrkDFK-yPLfcLkEkH3pyu--afLPr_k'; // Replace with your API key
 
 function fetchData() {
-    // Construct the URL for fetching the sheet data with the range parameter
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${sheetName}!${range}?key=${API_KEY}`;
-  
-    // Fetch the sheet data
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        // Define blue1 as the value of the 2nd row in column 4
-        const blue1 = data.values[1][3];
-        // Define b1Value as the value of cell C2
-        const b1Value = data.values[1][2];
-  
-        // Do something with blue1 and b1Value
-        console.log(blue1, b1Value);
-        updateB1Class(blue1, b1Value);
-  
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+  // Construct the URL for fetching the sheet data with the range parameter
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${sheetName}!${range}?key=${API_KEY}`;
 
-setInterval(fetchData, 10000); // Fetch data and console log every 10 seconds
+  // Fetch the sheet data
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // Define blue1 as the value of the 2nd row in column 4
+      const blue1 = data.values[1][3];
+      const blue1Item = data.values[1][2];
 
-function updateB1Class(blue1, b1Value) {
+      // Do something with blue1
+      console.log(blue1);
+      console.log(blue1Item);
+      updateB1Class(blue1, blue1Item);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+setInterval(fetchData, 2000); // Fetch data and console log every 10 seconds
+
+function updateB1Class(blue1, blue1Item) {
     const b1Div = document.getElementById('b1');
-    const b1ValueElem = document.getElementById('item-b1');
-  
+    const b1Text = document.getElementById('item-b1');
+    
     if (blue1 === '0') {
       b1Div.classList.remove('current');
       b1Div.classList.remove('bench2');
       b1Div.classList.add('bench0');
-      b1Div.style.animation = "pop-spin 1.5s forwards";
+      b1Div.style.animation = "pop-spin 1.2s forwards";
+      b1Text.innerText = blue1Item;
     } else {
       b1Div.classList.remove('bench0');
       b1Div.classList.add('bench2');
       b1Div.style.animation = "";
+      b1Text.innerText = "";
     }
-  
-    b1ValueElem.textContent = b1Value;
-  }
-
+}
