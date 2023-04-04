0460694ee86bd83ffdf70fc49274c572dace643c
Mitch Watson <user@Mitchs-MacBook-Pro.local>
2023-04-04 09:57:16 -0500

    added proper functioning

--- scripts.js
+++ scripts.js
@@ -12,36 +12,45 @@ function fetchData() {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      // Define blue1 as the value of the 2nd row in column 4
-      const blue1 = data.values[1][3];
-      const blue1Item = data.values[1][2];
-
-      // Do something with blue1
-      console.log(blue1);
-      console.log(blue1Item);
-      updateB1Class(blue1, blue1Item);
+      updateHTMLClasses(data);
     })
     .catch(error => {
       console.error(error);
     });
 }
 
-setInterval(fetchData, 2000); // Fetch data and console log every 10 seconds
+setInterval(fetchData, 2000); // Fetch data and update HTML every 2 seconds
 
-function updateB1Class(blue1, blue1Item) {
-    const b1Div = document.getElementById('b1');
-    const b1Text = document.getElementById('item-b1');
+function updateHTMLClasses(data) {
+  // Define an array of objects, each containing the ID of the HTML element and its corresponding value in the sheet
+  const elementData = [
+    { id: 'c1', value: data.values[34][3], item: data.values[34][2] },
+    { id: 'c2', value: data.values[35][3], item: data.values[35][2] },
+    { id: 'c3', value: data.values[36][3], item: data.values[36][2] },
+    { id: 'c4', value: data.values[37][3], item: data.values[37][2] },
+    { id: 'c5', value: data.values[38][3], item: data.values[38][2] },
+    { id: 'c6', value: data.values[39][3], item: data.values[39][2] },
+    { id: 'c7', value: data.values[40][3], item: data.values[40][2] },
+    { id: 'c8', value: data.values[41][3], item: data.values[41][2] }
     
-    if (blue1 === '0') {
-      b1Div.classList.remove('current');
-      b1Div.classList.remove('bench2');
-      b1Div.classList.add('bench0');
-      b1Div.style.animation = "pop-spin 1.2s forwards";
-      b1Text.innerText = blue1Item;
+  ];
+
+  // Loop through the array of objects and modify the class of each HTML element based on its value
+  for (let i = 0; i < elementData.length; i++) {
+    const element = document.getElementById(elementData[i].id);
+    const item = document.getElementById(`item-${elementData[i].id}`);
+    const value = elementData[i].value;
+    const itemValue = elementData[i].item;
+    if (value === '0') {
+      element.classList.remove('bench2');
+      element.classList.add('bench0');
+      item.innerHTML = itemValue;
+      element.style.animation = 'pop 0.7s ease-in-out forwards, shake 0.7s ease-in-out 0.7s infinite';
     } else {
-      b1Div.classList.remove('bench0');
-      b1Div.classList.add('bench2');
-      b1Div.style.animation = "";
-      b1Text.innerText = "";
+      element.classList.remove('bench0');
+      element.classList.add('bench2');
+      item.innerHTML = '';
+      element.style.animation = '';
     }
+  }
 }
